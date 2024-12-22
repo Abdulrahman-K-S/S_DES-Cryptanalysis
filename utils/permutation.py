c71298ad1861a5d2a3c3b55847db1a541eaa6a41
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-12-22 16:04:22 +0200

    feat (permutation): add permutation.py
    This file contains the functionality to perform the various permutations present in the S-DES algorithm.
    
    Functions present:
    - performPermutation(data, type)
       - This functions takes the data and the type of permutation to perform on the data and checks if the type is present. If it is it performs it & if it isn't then prints an error.

---
+++ utils/permutation.py
@@ -0,0 +1,39 @@
+"""permutation
+
+This file contains the functions that will perform
+the permutations on the bits of the plaintext & ciphertext
+"""
+
+import permutationMapping as pMapping
+
+
+def performPermutation(data, type):
+    """performPermutation
+
+    This functions takes the type of permutations to
+    perform and performs the permutation on the data given.
+
+    Arguments:
+        data (str): The data to perform the permutation on.
+        type (str): The type of permutation to perform.
+    
+    Return:
+        (str): The new data after the permutation.
+    """
+    mapping = None
+
+    if type == "initial":
+        mapping = pMapping.initial_permutation_map
+    elif type == "inverse":
+        mapping = pMapping.inverse_initial_permutation_map
+    elif type == "p4":
+        mapping = pMapping.permutation_4_map
+    elif type == "p4-inverse":
+        mapping = pMapping.permutation_4_inverse_map
+    elif type == "expansion":
+        mapping = pMapping.expansion_permutation_map
+
+    try:
+        return ''.join([data[i - 1] for i in mapping])
+    except:
+        print("Wrong permutation type!")
