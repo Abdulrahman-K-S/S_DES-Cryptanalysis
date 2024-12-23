591272ccad8091edb65051fb1b48b4ea7c972b20
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-12-23 21:57:03 +0200

    feat: add main function
    Now onto the main function, the flow of the program is as follows:
    1. Take the input from the user for both the known plaintext & ciphertext
    2. Find L0, R0 and Expanded_R0 from the top of round 1
    3. Find the possible inputs for K2 using R0 & the ciphertext in the second round
    4. Find the possible inputs for K1 using R2 & L0
    5. Calculate the possible keys for both K1 & K2
    6. Find the possible pairs
    7. Print the possible pairs if present
    
    Co-Authored-By: Mohamed Haytham <185723316+MoHaytham404@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from rounds import firstRoundFromTop, firstRoundFromBottom, calculateAllPossibleK1s, secondRoundFromBottom, secondRoundFromTop, calculateAllPossibleK2s
+from key_generation import findPossiblePairs
 
-plaintext=input("Enter plaintext:")
-ciphertext=input("Enter ciphertext:")
+if __name__ == "__main__":
+    plaintext=input("Enter plaintext:") # 00010110
+    ciphertext=input("Enter ciphertext:") # 10110111
 
+    L0, expanded_R0, R0 = firstRoundFromTop(plaintext)
+
+    round2_final_inputs, expanded_R1, R2 = secondRoundFromBottom(ciphertext, R0)
+
+    round1_final_inputs = firstRoundFromBottom(R2, L0)
+
+    possibleK1 = calculateAllPossibleK1s(round1_final_inputs, expanded_R0)
+    possibleK2 = calculateAllPossibleK2s(round2_final_inputs, expanded_R1)
+
+    possible_pairs = findPossiblePairs(possibleK1, possibleK2)
+    if possible_pairs:
+        for pair in possible_pairs:
+            print(pair)
