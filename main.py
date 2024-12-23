7fbd09860668342a6b07821c02bd4ea9c8c67fde
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-12-23 20:21:56 +0200

    build: moved main.py to root
    This way the main can see all the packages present
    
    Co-Authored-By: Mohamed Haytham <185723316+MoHaytham404@users.noreply.github.com>

---
+++ main.py
@@ -0,0 +1,5 @@
+from rounds import firstRoundFromTop, firstRoundFromBottom, calculateAllPossibleK1s, secondRoundFromBottom, secondRoundFromTop, calculateAllPossibleK2s
+
+plaintext=input("Enter plaintext:")
+ciphertext=input("Enter ciphertext:")
+
